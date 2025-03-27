55f6562f714fe504391bb0f834a2532fa012b7ea
kcohen1997 <57735569+kcohen1997@users.noreply.github.com>
2025-03-27 11:43:47 -0700

    Delete SQL Queries/Create Product List Table.sql

--- SQL Queries/Create Product List Table.sql
+++
@@ -1,3 +0,0 @@
-SELECT [Handle], [Title], [Status], [Published], [Body (HTML)], [Option1 Name], [Option2 Name], [Option3 Name], [Vendor], [Product Category], [Type], [Gift Card], [Included / International], [SEO Title], [SEO Description], [Google Shopping / Google Product Category], [Tags] INTO [Product List]
-FROM products_export_1
-WHERE [Status] <> '';
